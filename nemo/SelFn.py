d2bf4a7f2d5abb2c3ceb28aae3315fdb780bd0f8
Matt Hilton <matt.hilton@mykolab.com>
2017-03-31 09:30:15 +0200

    Now calculating M500 detection probabilities correctly in SelFn, and can extract these at given RADeg, decDeg coordinates if needed, or use survey-wide average. Added routines to SelFn to calculate mass limit at given completeness level. This is now called by nemoSelFn to make the survey-average M vs z completeness plot.

--- nemo/SelFn.py
+++ nemo/SelFn.py
@@ -1,6 +1,6 @@
 """
 
-A class which handles application of the selection function, as calculated by nemoSelFn
+A class which handles application of the selection function, as calculated by nemoSelFn.
 
 
 """
@@ -10,8 +10,10 @@ import sys
 import numpy as np
 import IPython
 import astropy.table as atpy
+from astLib import *
 import pylab as plt
 from scipy import interpolate
+from scipy import stats
 from astLib import *
 plt.matplotlib.interactive(False)
 
@@ -30,38 +32,83 @@ class SelFn(object):
         """
         
         self.fitTab=atpy.Table().read(nemoOutputDir+os.path.sep+"diagnostics"+os.path.sep+"fitSimTab.fits")
+        self.wcs=astWCS.WCS(nemoOutputDir+os.path.sep+"diagnostics"+os.path.sep+"areaMask.fits")
         self.MLimits=self.fitTab['MFitSlope']*SNRCut+self.fitTab['MFitIntercept']
         self.ycLimits=self.fitTab['ycFitSlope']*SNRCut+self.fitTab['ycFitIntercept']
         self.SNRCut=SNRCut
+        self.zRange=np.array(np.unique(self.fitTab['z']))
+        
+        # Calculate survey-averaged completeness (weighted by area of each cell)
+        self.surveyAverageMLimit=np.zeros(self.zRange.shape)
+        for i in range(self.zRange.shape[0]):
+            z=self.zRange[i]
+            mask=np.equal(self.fitTab['z'], z)
+            self.surveyAverageMLimit[i]=np.average(self.MLimits[mask], weights = self.fitTab['fracSurveyArea'][mask])
+        
+        # Retrieves the survey-averaged mass limit at specified z, using a spline interpolation.
+        self.getSurveyAverageMLimitAtRedshift=interpolate.InterpolatedUnivariateSpline(self.zRange, self.surveyAverageMLimit)
+
+
+    def findCell(self, RADeg, decDeg):
+        """Returns the ID in self.fitTab of the cell that contains the given RA, dec coordinates.
+        
+        """
+        x, y=self.wcs.wcs2pix(RADeg, decDeg)
+        xMask=np.logical_and(np.greater(x, self.fitTab['x0']), np.less(x, self.fitTab['x1']))
+        yMask=np.logical_and(np.greater(y, self.fitTab['y0']), np.less(y, self.fitTab['y1']))
+        mask=np.logical_and(xMask, yMask)
+        
+        return int(self.fitTab['ID'][mask][0])
+
     
-        
-    def M500Completeness(self, M500, z):
-        """Returns completeness for cluster of given M500 (in 10^14 MSun) at given z. This could also be 
-        thought of as detection probability.
-        
-        Note that z can be an array, but M500 cannot.
+    def surveyAverageMLimitAtCompleteness(self, completenessLim):
+        """Returns the survey-averaged M500 limit and the corresponding zs at which it is evaluated at the 
+        given completeness level.
         
         """
         
-        # This is too slow currently
-        zRange=np.unique(self.fitTab['z'])
-        completeness=np.zeros(zRange.shape)
-        for i in range(zRange.shape[0]):
-            zMask=np.equal(self.fitTab['z'], zRange[i])
-            selMask=np.less(self.MLimits[zMask], M500)
-            # Weighted by area of each tile
-            completeness[i]=np.sum(self.fitTab['fracSurveyArea'][zMask][selMask])/np.sum(self.fitTab['fracSurveyArea'][zMask])
-        tck=interpolate.splrep(zRange, completeness)
+        for m in np.linspace(1.0, 2.0, 1001):
+            testComplete=np.mean(stats.norm.cdf(np.log10(self.surveyAverageMLimit*m), loc = np.log10(self.surveyAverageMLimit), 
+                                                scale = np.log10(1.+1./self.SNRCut)))
+            if testComplete > completenessLim:
+                break
         
-        # Sanity check plot
-        #plotRange=np.linspace(zRange.min(), zRange.max(), 1000)
-        #plt.plot(zRange, completeness, 'r.')
-        #plt.plot(plotRange, interpolate.splev(plotRange, tck), 'k-')
-        #plt.xlabel("z")
-        #plt.ylabel("completeness")
-        #plt.title("M500 = %.2f x 10$^{14}$ M$_{\odot}$, z = %.3f" % (M500, z))
+        return np.power(10, np.log10(self.surveyAverageMLimit*m)), self.zRange
         
-        return interpolate.splev(z, tck)
+        
+    def logM500DetectionProbability(self, logM500, z, logM500Err, RADeg = None, decDeg = None):
+        """Returns detection probability and mass limit for cluster of given log10(M500 / 10^14 MSun) at given z. 
+        This could also be thought of as completeness.
+        
+        For real clusters, nemoMass gives M500, M500_errPlus, M500_errMinus, which can be used to feed in logM500Err
+        (error bars from nemoMass are log normal). For simulated clusters drawn from the mass function (using 
+        MockSurvey), set logM500Err according to the SNR.
+        
+        If RADeg, decDeg are given, the probability and mass limit is evaluated at that position. Otherwise, the
+        survey-averaged detection probability is returned.
+        
+        """
+               
+        # Given position
+        if np.any(RADeg) != None and np.any(decDeg) != None:
+            # These are over the range of zs in the tile we're trying
+            cellID=self.findCell(RADeg, decDeg)
+            cellMLimits=self.MLimits[np.equal(self.fitTab['ID'], cellID)]
+            tck=interpolate.splrep(self.zRange, cellMLimits)
+            MLimitAtClusterRedshift=interpolate.splev(z, tck)
+            # Sanity check plot
+            #plt.plot(np.unique(self.fitTab['z']), cellMLimits, 'r.')
+            #plotRange=np.linspace(0, 2, 100)
+            #plt.plot(plotRange, interpolate.splev(plotRange, tck), 'k-')
+            
+        # Survey-wide average
+        else:
+            MLimitAtClusterRedshift=self.getSurveyAverageMLimitAtRedshift(z)
+
+        # Defining detP as the fraction of the M500 probability distribution that's over the mass selection limit
+        detP=stats.norm.sf(np.log10(MLimitAtClusterRedshift), loc = logM500, scale = logM500Err)
+        
+        return detP, MLimitAtClusterRedshift
     
     
     def y0Completeness(self, y0, z):
@@ -70,7 +117,13 @@ class SelFn(object):
         
         Note that z can be an array, but M500 cannot.
 
+        NOTE: This routine is also currently wrong and hasn't been fixed yet...
+        
         """
+        
+        print "Fix y0completeness calc"
+        IPython.embed()
+        sys.exit()
         
         # We could avoid some duplication here...
         zRange=np.unique(self.fitTab['z'])
@@ -86,7 +139,10 @@ class SelFn(object):
             
             
     def makeM500LimitMap(self, z, wcs, outFileName):
-        """Makes a .fits image map of the M500 completeness limit.
+        """Makes a .fits image map of the M500 completeness limit. 
+        
+        By definition, the limit is 50% completeness at whatever SNRCut was initially specified for the
+        selFn object.
         
         """
         
