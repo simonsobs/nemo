Big overhaul/bug fixes to selection function calculation
Most of the work done in MockSurvey.drawSample, which is now used
everywhere in selection function calculation. Can now make mock
catalogs in E-D56 field in 10 sec or less.

Changed calcCompleteness to now generate the (M, z) grid itself.
Much faster than previous method and fixed a bug there (it was not
correct before).

Adapted nemoSelFn, nemoMock, nemoCosmo scripts accordingly.

nemo/MockSurvey.py
@@ -18,6 +18,7 @@ from . import simsTools
 from . import catalogTools
 import pickle
 from scipy import interpolate
+from scipy.interpolate import InterpolatedUnivariateSpline as _spline
 from scipy import stats
 from astLib import *
 import time
@@ -41,8 +42,8 @@ class MockSurvey(object):
         # Globally change hmf's cosmology - at least, according to the docs...
         # NOTE: for astropy 2.0+ need to actually set Tcmb0 here as it defaults to zero
         # Here we use the value from Fixsen (2009): http://adsabs.harvard.edu/abs/2009ApJ...707..916F
-        cosmo_model=FlatLambdaCDM(H0 = H0, Om0 = Om0, Ob0 = Ob0, Tcmb0 = 2.72548)
-        cosmo.Cosmology(cosmo_model = cosmo_model)
+        self.cosmo_model=FlatLambdaCDM(H0 = H0, Om0 = Om0, Ob0 = Ob0, Tcmb0 = 2.72548)
+        cosmo.Cosmology(cosmo_model = self.cosmo_model)
         
         # For drawSample, we use astLib routines (a few times faster than cosmo_model)
         # This is just to make sure we used the same parameters
@@ -58,17 +59,17 @@ class MockSurvey(object):
         # Internally, it's better to stick with how hmf does this, i.e., use these units
         # Externally, we still give  inputs without h^-1
         self.mf=hmf.MassFunction(z = zRange[0], Mmin = 13., Mmax = 16., delta_wrt = 'crit', delta_h = 500.0,
-                                 sigma_8 = sigma_8, cosmo_model = cosmo_model)#, force_flat = True, cut_fit = False)
+                                 sigma_8 = sigma_8, cosmo_model = self.cosmo_model)#, force_flat = True, cut_fit = False)
             
         self.log10M=np.log10(self.mf.m/self.mf.cosmo.h)
         self.areaSr=areaSr
+        self.areaDeg2=areaDeg2
         self.zBinEdges=zRange
         self.z=(zRange[:-1]+zRange[1:])/2.
 
         self.enableDrawSample=enableDrawSample
         self.update(H0, Om0, Ob0, sigma_8)
         
-                    
             
     def update(self, H0, Om0, Ob0, sigma_8):
         """Recalculate cluster counts if cosmological parameters updated.
@@ -94,31 +95,22 @@ class MockSurvey(object):
         # Stuff to enable us to draw mock samples (see drawSample)
         # Interpolators here need to be updated each time we change cosmology
         if self.enableDrawSample == True:
+
+            # Now using hmf.sample style...
             # For drawing from overall z distribution
             zSum=self.clusterCount.sum(axis = 1)
             pz=np.cumsum(zSum)/self.numClusters
-            self.tck_zRoller=interpolate.splrep(pz, self.z)
+            self.zRoller=_spline(pz, self.z, k = 3)
+            
             # For drawing from each log10M distribution at each point on z grid
-            # This can blow up if there are identical entries in pM... so we truncate spline fit when reach 1.0
-            # NOTE: Should add sanity check code that the interpolation used here is accurate (make plots)
-            #print "WARNING: should add code to check interpolation used for drawing mock samples is accurate enough"
-            self.tck_log10MRoller=[]
+            self.log10MRollers=[]
             for i in range(len(self.z)):
-                MSum=self.clusterCount[i].sum()
-                pM=np.cumsum(self.clusterCount[i])/MSum
-                # To avoid multiple pM == 1 blowing up the spline fit
-                try:
-                    maxIndex=np.min(np.where(np.diff(pM) == 0))
-                except:
-                    maxIndex=len(pM)
-                self.tck_log10MRoller.append(interpolate.splrep(pM[:maxIndex], self.log10M[:maxIndex]))
-            # Sanity check
-            for i in range(len(self.z)):
-                if np.any(np.isnan(self.tck_log10MRoller[i][1])) == True:
-                    print("nans in self.tck_log10MRoller[%d]" % (i))
-                    IPython.embed()
-                    sys.exit()
-        
+                #print("updating z = %.2f" % (self.z[i]))
+                self.mf.update(z = self.z[i])
+                mask=self.mf.ngtm > 0
+                # NOTE: / h here to match what we do in cluster count (also ensures we don't need to worry about little h in draw sample)
+                self.log10MRollers.append(_spline((self.mf.ngtm[mask] / self.mf.ngtm[0])[::-1], np.log10(self.mf.m[mask][::-1]/self.mf.cosmo.h), k=3))
+                
 
     def _doClusterCount(self):
         """Updates cluster count etc. after mass function object is updated.
@@ -127,7 +119,7 @@ class MockSurvey(object):
         
         mf=self.mf
         zRange=self.zBinEdges
-    
+            
         # Number density by z and total cluster count (in redshift shells)
         # Can use to make P(m, z) plane
         numberDensity=[]
@@ -188,13 +180,17 @@ class MockSurvey(object):
         PLog10M=numberDensity/np.trapz(numberDensity, self.mf.m)
 
         return PLog10M
-    
-    
-    def drawSample(self, SNRLimit, areaMask, RMSMap, wcs, scalingRelationDict, tckQFitDict, 
-                   photFilterLabel = None, extName = None):
+
+
+    def drawSample(self, y0Noise, scalingRelationDict, tckQFitDict, wcs = None, photFilterLabel = None, 
+                   extName = None, SNRLimit = None, makeNames = False, z = None, numDraws = None,
+                   areaDeg2 = None, applySNRCut = False):
         """Draw a cluster sample from the mass function, generate mock y0~ values by applying the given 
-        scaling relation parameters, and then apply the survey selection function using the given
-        SNRLimit.
+        scaling relation parameters, and then (optionally, if both SNRLimit is given and applySNRCut 
+        is True) apply the survey selection function.
+        
+        Here, y0Noise can be either a single number (if using e.g. survey average) or an RMSMap
+        (2d array). Coords will be generated if an RMSMap and a wcs are supplied.
         
         scalingRelationDict should contain keys 'tenToA0', 'B0', 'Mpivot', 'sigma_int' (this is the
         contents of massOptions in nemo .yml config files).
@@ -205,80 +201,185 @@ class MockSurvey(object):
         photFilterLabel and extName are only used for adding a 'template' key to each object: useful
         for quickly checking which tile (extName) an object is in.
         
+        If z is given, the sample is drawn from only that redshift. The default (z = None) is to use
+        the range given by self.z
+        
+        If numDraws = None, the number of draws is set by self.numClustersByRedshift. If numDraws is
+        given, the number of draws will be divided equally between each z. If this option is used,
+        an approximation for the lower mass limit will be calculated (given the noise) to better
+        sample the parameter space above the SNRLimit.
+        
+        if areaDeg2 is given, the cluster counts will be scaled accordingly (otherwise, they 
+        correspond to self.areaDeg2). This will be ignored if numDraws is also set.
+        
         This routine is used in the nemoMock script.
 
-        Returns a catalog (list containing object dictionaries).
+        Returns catalog as an astropy Table (and an array of length self.z corresponding to low mass limit
+        if numDraws is used).
                 
         """
         
+        if z == None:
+            zRange=self.z
+            numClusters=int(round(self.numClustersByRedshift.sum()))
+        else:
+            # Pick the nearest z on the grid
+            zIndex=np.argmin(abs(z-self.z))
+            zRange=[self.z[zIndex]] 
+            numClusters=int(round(self.numClustersByRedshift[zIndex]))
+        
+        if areaDeg2 != None:
+            numClusters=int(round(numClusters*(areaDeg2/self.areaDeg2)))
+
+        if numDraws != None:
+            numClusters=numDraws            
+
         tenToA0, B0, Mpivot, sigma_int=[scalingRelationDict['tenToA0'], scalingRelationDict['B0'], 
                                         scalingRelationDict['Mpivot'], scalingRelationDict['sigma_int']]
-            
-        numClusters=int(round(self.numClusters))
+                    
+        # If given y0Noise as RMSMap, draw coords (assuming clusters aren't clustered - which they are...)
+        # NOTE: switched to using valid part of RMSMap here rather than areaMask - we need to fix the latter to same area
+        # It isn't a significant issue though
+        if type(y0Noise) == np.ndarray and y0Noise.ndim == 2:
+            RMSMap=y0Noise
+            ysInMask, xsInMask=np.where(RMSMap != 0)
+            coordIndices=np.random.randint(0, len(xsInMask), numClusters)
+            ys=ysInMask[coordIndices]
+            xs=xsInMask[coordIndices]
+            if wcs != None:
+                RADecCoords=wcs.pix2wcs(xs, ys)
+                RADecCoords=np.array(RADecCoords)
+                RAs=RADecCoords[:, 0]
+                decs=RADecCoords[:, 1]
+            y0Noise=RMSMap[ys, xs]
+        else:
+            y0Noise=np.ones(numClusters)*y0Noise
+            RAs=np.zeros(numClusters)
+            decs=np.zeros(numClusters)
         
-        # Draw coords (assuming clusters aren't clustered - which they are...)
-        ysInMask, xsInMask=np.where(areaMask != 0)
-        coordIndices=np.random.randint(0, len(xsInMask), numClusters)
-        ys=ysInMask[coordIndices]
-        xs=xsInMask[coordIndices]
-        RADecCoords=wcs.pix2wcs(xs, ys)
-        RADecCoords=np.array(RADecCoords)
-        RAs=RADecCoords[:, 0]
-        decs=RADecCoords[:, 1]
-
-        # Roll for redshifts
-        zs=interpolate.splev(np.random.uniform(0, 1, numClusters), self.tck_zRoller)
-                
-        # Draw from mass function - takes ~6 sec for E-D56
-        log10Ms=[]
+        # Fancy names or not?
+        if makeNames == True:
+            names=[]
+            for RADeg, decDeg in zip(RAs, decs):
+                names.append(catalogTools.makeACTName(RADeg, decDeg, prefix = 'MOCK-CL'))
+        else:
+            names=np.arange(numClusters)+1
+        
+        # New way - on the redshift grid
         t0=time.time()
-        for z in zs:
-            # Draw M|z
-            zIndex=np.where(abs(self.z-z) == abs(self.z-z).min())[0][0]
-            MRoll=np.random.uniform(0, 1)
-            log10M=interpolate.splev(MRoll, self.tck_log10MRoller[zIndex])
-            log10Ms.append(float(log10M))
-        log10Ms=np.array(log10Ms)
-        t1=time.time()
-        
-        # Mock observations - takes < 1 min for E-D56
-        # NOTE: we need to re-jig where fRel is applied generally for multi-freq analysis
-        mockCatalog=[]
-        for x, y, RADeg, decDeg, z, log10M in zip(xs, ys, RAs, decs, zs, log10Ms):
-            # NOTE: Cosmology changes from astLib defaults handled in __init__
-            # We're still using astCalc here as it's several times faster for da than cosmo_model above
-            Ez=astCalc.Ez(z)
-            Hz=Ez*astCalc.H0
+        currentIndex=0
+        measured_y0s=np.zeros(y0Noise.shape)
+        true_y0s=np.zeros(y0Noise.shape)
+        log10Ms=np.zeros(y0Noise.shape)
+        zs=np.zeros(y0Noise.shape)
+        zErrs=np.zeros(y0Noise.shape)
+        minTrueMass=np.zeros(len(zRange))   # if using numDraws
+        for k in range(len(zRange)):
+            
+            zk=zRange[k]
+            zIndex=np.argmin(abs(zk-self.z))      
+            if numDraws != None:
+                numClusters_zk=int(round(numDraws/len(zRange)))
+            else:
+                numClusters_zk=int(round(self.numClustersByRedshift[zIndex]))
+            if numClusters_zk == 0:
+                continue
+            
+            # Some fiddling here to avoid rounding issues with array sizes (+/-1 draw here shouldn't make a difference)
+            nextIndex=currentIndex+numClusters_zk
+            if nextIndex >= len(y0Noise):
+                nextIndex=len(y0Noise)-1
+            mask=np.arange(currentIndex, nextIndex)
+            numClusters_zk=len(mask)
+            y0Noise_zk=y0Noise[mask]
+            currentIndex=nextIndex
+            
+            # For quick Q, fRel calc (this bit takes ~0.01 sec)
+            Ez=astCalc.Ez(zk)
+            Hz=astCalc.Ez(zk)*astCalc.H0  
             G=4.301e-9  # in MSun-1 km2 s-2 Mpc
             criticalDensity=(3*np.power(Hz, 2))/(8*np.pi*G)
-            M500=np.power(10, log10M)
-            R500Mpc=np.power((3*M500)/(4*np.pi*500*criticalDensity), 1.0/3.0)                     
-            theta500Arcmin=np.degrees(np.arctan(R500Mpc/astCalc.da(z)))*60.0
-            Q=interpolate.splev(theta500Arcmin, tckQFitDict[extName])
-            fRel=simsTools.calcFRel(z, M500)
-            true_y0=tenToA0*np.power(Ez, 2)*np.power(M500/Mpivot, 1+B0)*Q*fRel   
-            if true_y0 < 0:
-                continue    # This happens if extremely low mass / wander out of range where Q fit is valid
-            # Mock "observations" (apply intrinsic scatter and noise)...
-            scattered_y0=np.exp(np.random.normal(np.log(true_y0), sigma_int))        
-            y0Noise=RMSMap[y, x]
-            measured_y0=np.random.normal(scattered_y0, y0Noise)
-            if measured_y0 > y0Noise*SNRLimit and y0Noise > 0:
-                name=catalogTools.makeACTName(RADeg, decDeg, prefix = 'MOCK-CL')
-                objDict={'name': name, 
-                         'RADeg': RADeg,
-                         'decDeg': decDeg,
-                         'true_M500': M500/1e14,
-                         'true_fixed_y_c': true_y0/1e-4,
-                         'fixed_y_c': measured_y0/1e-4,
-                         'fixed_err_y_c': y0Noise/1e-4,
-                         'fixed_SNR': measured_y0/y0Noise,
-                         'redshift': z,
-                         'redshiftErr': 0}
-                # Usefull to quickly check which tile something is in
-                if photFilterLabel != None and extName != None:
-                    objDict['template']=photFilterLabel+"#"+extName
-                mockCatalog.append(objDict)
+            interpLim_minLog10M=self.log10M.min()
+            interpLim_maxLog10M=self.log10M.max()
+            interpPoints=100
+            fitM500s=np.power(10, np.linspace(interpLim_minLog10M, interpLim_maxLog10M, interpPoints))
+            fitTheta500s=np.zeros(len(fitM500s))
+            fitFRels=np.zeros(len(fitM500s))
+            DA=astCalc.da(zk)
+            Ez=astCalc.Ez(zk)
+            for i in range(len(fitM500s)):
+                M500=fitM500s[i]
+                R500Mpc=np.power((3*M500)/(4*np.pi*500*criticalDensity), 1.0/3.0)                     
+                theta500Arcmin=np.degrees(np.arctan(R500Mpc/DA))*60.0
+                fitTheta500s[i]=theta500Arcmin
+                fitFRels[i]=simsTools.calcFRel(zk, M500)
+            tckLog10MToTheta500=interpolate.splrep(np.log10(fitM500s), fitTheta500s)
+            tckLog10MToFRel=interpolate.splrep(np.log10(fitM500s), fitFRels)
+        
+            # Calc max draw (turns out we need bounds checking on spline here also)
+            if numDraws != None:
+                test_draws=np.linspace(0, 1, 10000)
+                test_log10Ms=self.log10MRollers[k](test_draws)
+                test_log10Ms[test_log10Ms < self.log10M.min()]=self.log10M.min()
+                test_log10Ms[test_log10Ms > self.log10M.max()]=self.log10M.max()
+                test_theta500s=interpolate.splev(test_log10Ms, tckLog10MToTheta500)
+                test_Qs=interpolate.splev(test_theta500s, tckQFitDict[extName])
+                test_fRels=interpolate.splev(test_log10Ms, tckLog10MToFRel)  
+                test_y0s=tenToA0*np.power(Ez, 2)*np.power(np.power(10, test_log10Ms)/Mpivot, 1+B0)*test_Qs*test_fRels
+                #maxDraw=test_draws[np.argmin(abs(test_y0s-((SNRLimit-3)*y0Noise.min())))]
+                maxDraw=test_draws[np.argmin(abs(test_y0s-((SNRLimit-2.5)*y0Noise.min())))]
+                minTrueMass[k]=self.log10MRollers[k](maxDraw)
+            else:
+                maxDraw=1.0
+            
+            # For some cosmo parameters, splined masses can end up outside of valid range, so catch this
+            log10Ms_zk=self.log10MRollers[k](np.random.uniform(0, maxDraw, numClusters_zk))
+            log10Ms_zk[log10Ms_zk < self.log10M.min()]=self.log10M.min()
+            log10Ms_zk[log10Ms_zk > self.log10M.max()]=self.log10M.max()
+            
+            theta500s_zk=interpolate.splev(log10Ms_zk, tckLog10MToTheta500)
+            Qs_zk=interpolate.splev(theta500s_zk, tckQFitDict[extName])
 
-        return mockCatalog
+            # For some cosmo parameters, fRel can wander outside its range for crazy masses
+            # So we just cap it at 0.1 here just to avoid -ve in log
+            fRels_zk=interpolate.splev(log10Ms_zk, tckLog10MToFRel)
+            fRels_zk[fRels_zk <= 0]=0.1
+            fRels_zk[fRels_zk > 1]=1.0
+            
+            try:
+                true_y0s_zk=tenToA0*np.power(Ez, 2)*np.power(np.power(10, log10Ms_zk)/Mpivot, 1+B0)*Qs_zk*fRels_zk
+                scattered_y0s_zk=np.exp(np.random.normal(np.log(true_y0s_zk), sigma_int, len(true_y0s_zk)))        
+                measured_y0s_zk=np.random.normal(scattered_y0s_zk, y0Noise_zk)
+            except:
+                raise Exception("Negative y0 values (probably spline related) for H0 = %.6f Om0 = %.6f sigma_8 = %.6f at z = %.3f" % (self.H0, self.Om0, self.sigma_8, zk))
+                
+            true_y0s[mask]=true_y0s_zk
+            measured_y0s[mask]=measured_y0s_zk
+            log10Ms[mask]=log10Ms_zk
+            zs[mask]=zk
+        
+        # Make table
+        tab=atpy.Table()
+        tab.add_column(atpy.Column(names, 'name'))
+        tab.add_column(atpy.Column(RAs, 'RADeg'))
+        tab.add_column(atpy.Column(decs, 'decDeg'))
+        tab.add_column(atpy.Column(np.power(10, log10Ms)/1e14, 'true_M500'))
+        tab.add_column(atpy.Column(true_y0s/1e-4, 'true_fixed_y_c'))
+        tab.add_column(atpy.Column(measured_y0s/1e-4, 'fixed_y_c'))
+        tab.add_column(atpy.Column(y0Noise/1e-4, 'fixed_err_y_c'))
+        tab.add_column(atpy.Column(measured_y0s/y0Noise, 'fixed_SNR'))
+        tab.add_column(atpy.Column(zs, 'redshift'))
+        tab.add_column(atpy.Column(zErrs, 'redshiftErr'))
+        if photFilterLabel != None and extName != None:
+            tab.add_column(atpy.Column([photFilterLabel+"#"+extName]*len(tab), 'template'))
+
+        # Apply selection?
+        if applySNRCut == True:
+            selMask=measured_y0s > y0Noise*SNRLimit
+            tab=tab[selMask]
+
+        if numDraws != None:
+            return tab, minTrueMass
+        else:
+            return tab
         

nemo/SelFn.py
@@ -111,11 +111,39 @@ class SelFn(object):
         compMzCube=[]
         for selFnDict in self.selFnDictList:
             tileAreas.append(selFnDict['tileAreaDeg2'])
-            selFnDict['fitTab']=selFnTools.calcCompleteness(selFnDict['y0Noise'], self.SNRCut, selFnDict['extName'], self.mockSurvey, 
-                                                            self.scalingRelationDict, self.tckQFitDict, self.diagnosticsDir)
-            compMzCube.append(selFnTools.makeMzCompletenessGrid(selFnDict['fitTab'], self.mockSurvey))
+            selFnDict['compMz']=selFnTools.calcCompleteness(selFnDict['y0Noise'], self.SNRCut, selFnDict['extName'], self.mockSurvey, 
+                                                            self.scalingRelationDict, self.tckQFitDict)
+            compMzCube.append(selFnDict['compMz'])
         tileAreas=np.array(tileAreas)
         fracArea=tileAreas/self.totalAreaDeg2
         compMzCube=np.array(compMzCube)
         self.compMz=np.average(compMzCube, axis = 0, weights = fracArea)
 
+
+    def projectCatalogToMz(self, tab):
+        """Project a catalog (as astropy Table) into the (log10 M500, z) grid. Takes into account the uncertainties
+        on y0, redshift - but if redshift error is non-zero, is a lot slower.
+        
+        Returns (log10 M500, z) grid
+        
+        """
+        
+        catProjectedMz=np.zeros(self.compMz.shape)
+        tenToA0, B0, Mpivot, sigma_int=self.scalingRelationDict['tenToA0'], self.scalingRelationDict['B0'], \
+                                       self.scalingRelationDict['Mpivot'], self.scalingRelationDict['sigma_int']
+        for row in tab:
+            extName=row['template'].split("#")[-1]
+            z=row['redshift']
+            zErr=row['redshiftErr']
+            y0=row['fixed_y_c']*1e-4
+            y0Err=row['fixed_err_y_c']*1e-4
+            P=simsTools.calcPM500(y0, y0Err, z, zErr, self.tckQFitDict[extName], self.mockSurvey, 
+                                  tenToA0 = tenToA0, B0 = B0, Mpivot = Mpivot, sigma_int = sigma_int, 
+                                  applyMFDebiasCorrection = True, fRelWeightsDict = {148.0: 1.0},
+                                  return2D = True)
+            # Paste into (M, z) grid
+            catProjectedMz=catProjectedMz+P # For return2D = True, P is normalised such that 2D array sum is 1
+        
+        return catProjectedMz
+
+    