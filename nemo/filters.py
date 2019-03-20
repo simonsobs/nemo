30997cfd37107d555b95db260c54aba21c5f87e7
Matt Hilton <mattyowl@gmail.com>
2019-03-20 16:08:21 +0200

    Removed a stray image writing call (debugging related)

--- nemo/filters.py
+++ nemo/filters.py
@@ -457,7 +457,7 @@ class MatchedFilter(MapFilter):
                     else:
                         raise Exception("Other noise models not yet re-implemented")
                     NP=ndimage.gaussian_filter(NP, kernelSize)
-                    astImages.saveFITS("test_%d_%d_%s.fits" % (i,  j, self.extName), fft.fftshift(NP), None)
+                    #astImages.saveFITS("test_%d_%d_%s.fits" % (i,  j, self.extName), fft.fftshift(NP), None)
                     row.append(NP)
                 noiseCov.append(row)
             del fMapsForNoise
