e4ddb75c51eecc524df7c82b90da97cb68e8d43e
Matt Hilton <matt.hilton@mykolab.com>
2020-06-26 07:45:36 +0200

    WebSky cosmology in SOSim configs and updated slurm config for hippo (anaconda)

--- examples/SOSims/slurm_mass.sh
+++ examples/SOSims/slurm_mass.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-#SBATCH --nodes=5
+#SBATCH --nodes=10
 #SBATCH --ntasks-per-node=20
 #SBATCH --mem=64000
 #SBATCH --time=03:30:00
 
-source ~/.bashrc
+#source ~/.bashrc
+source /home/mjh/SETUP_CONDA.sh
 time mpiexec nemoMass MFMF_SOSim_3freq_tiles.yml -M
