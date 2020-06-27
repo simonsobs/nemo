22bc2512617a12bbdafec542fd45b555c6f83227
Matt Hilton <mattyowl@gmail.com>
2020-06-27 20:58:20 +0200

    Oops... missing mpiexec

--- examples/SOSims/slurm_small_cosmo.sh
+++ examples/SOSims/slurm_small_cosmo.sh
@@ -5,6 +5,5 @@
 #SBATCH --time=23:59:00
 
 source /home/mjh/SETUP_CONDA.sh
-time nemoCosmo mocks_small/mockCatalog_1.fits MFMF_SOSim_3freq_small/selFn -c cobayaConf_SOSims_fixedScalingRelation.yml -S 5 -o small_cosmoResults1
-#time mpiexec nemoCosmo mocks_small/mockCatalog_1.fits MFMF_SOSim_3freq_small/selFn
+time mpiexec nemoCosmo mocks_small/mockCatalog_1.fits MFMF_SOSim_3freq_small/selFn -c cobayaConf_SOSims_fixedScalingRelation.yml -S 5 -o small_cosmoResults1
 
