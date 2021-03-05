b2cb1d730f04cae49a39ad08602862c78b4af151
Matt Hilton <matt.hilton@mykolab.com>
2021-03-05 11:32:49 +0200

    DR5 tutorial stuff

--- examples/ACT-DR5-clusters/FETCH_MAPS.sh
+++ examples/ACT-DR5-clusters/FETCH_MAPS.sh
@@ -1,7 +1,11 @@
 mkdir -p maps
 cd maps
-wget https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr5/maps/act_dr5.01_s08s18_AA_f090_daynight_map.fits
-wget https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr5/maps/act_dr5.01_s08s18_AA_f090_daynight_ivar.fits
-wget https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr5/maps/act_dr5.01_s08s18_AA_f150_daynight_map.fits
-wget https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr5/maps/act_dr5.01_s08s18_AA_f150_daynight_ivar.fits
+wget -c https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr5/maps/act_dr5.01_s08s18_AA_f090_daynight_map.fits
+wget -c https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr5/maps/act_dr5.01_s08s18_AA_f090_daynight_ivar.fits
+wget -c https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr5/maps/act_dr5.01_s08s18_AA_f150_daynight_map.fits
+wget -c https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr5/maps/act_dr5.01_s08s18_AA_f150_daynight_ivar.fits
 cd ..
+wget -c https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr5/maps/act_dr5.01_auxilliary.zip
+unzip act_dr5.01_auxilliary.zip
+wget -c https://astro.ukzn.ac.za/~mjh/nemo-dr5-masking.tar.gz
+tar -zxvf nemo-dr5-masking.tar.gz
